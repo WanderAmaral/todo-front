0d617ff04edf0e294e8f51f7f982a7622b6ac4dc
Wander Amaral <wanderamaral013@gmail.com>
2025-03-01 22:43:34 -0300

    commit

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,48 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { CirclePlus } from "lucide-react";
+import Image from "next/image";
 
-export default function Home() {
-  return <>
-  
-  </>;
+export default async function Home() {
+  return (
+    <div className="h-screen">
+      <div className="bg-[#0D0D0D] h-[250px]">
+        <div className=" max-w-[763px] mx-auto">
+          <div className="flex justify-center py-[87px]">
+            <Image
+              src={"/Logo.png"}
+              alt="Logo"
+              width={0}
+              height={0}
+              className="w-32"
+              sizes="100vw"
+            />
+          </div>
+
+          <div className="flex gap-4 w-full items-center">
+            <Input
+              placeholder="Adicione uma nova tarefa"
+              className="bg-[#262626] p-6 border-none"
+            />
+            <Button className="p-6 bg-blue-dark">
+              Criar <CirclePlus />
+            </Button>
+          </div>
+
+          <div className="flex justify-between pt-20">
+            <div className="flex items-center gap-3">
+              <span className="text-blue">Tarefas concluidas</span>
+              <Badge className="bg-[#333333] rounded-full">5</Badge>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <span className="text-purple">Concluidas</span>
+              <Badge className="bg-[#333333] rounded-full">2 de 5</Badge>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,23 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import Link from "next/link";
 
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="w-full max-w-md p-6 bg-white shadow-lg rounded-lg">
-        <h2 className="text-2xl font-bold text-center text-gray-800">Entrar</h2>
+        <h2 className="text-2xl font-bold text-center text-gray-800">
+          Cadastre-se
+        </h2>
         <form className="mt-4 space-y-4">
+          <Input type="email" placeholder="Nome" className="w-full" />
           <Input type="email" placeholder="E-mail" className="w-full" />
           <Input type="password" placeholder="Senha" className="w-full" />
-          <Button className="w-full">Entrar</Button>
+          <Button type="button" className="w-full">Criar conta</Button>
         </form>
-        <p className="mt-4 text-center text-sm text-gray-600">
-          Não tem uma conta?
-          <Link href={"/signup"} className="text-blue-600 hover:underline ml-2">
-            Cadastre-se
-          </Link>
-        </p>
       </div>
     </div>
   );
